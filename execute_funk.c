d300d84d4dfbd18f91baad8ec4989589968c13ea
MoeAlAnnan <5653@holbertonstudents.com>
2022-12-06 09:40:26 -0800

    version-0 execute function

---
+++ execute_funk.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include "shell.h"
+/**
+ * execute_funk - a function
+ * @argv: it is the return value of function token_creator
+ * @argenv: is the current environment
+ * Description: a function that forks and execite
+ * Return: i still dont know
+ */
+void execute_funk(char **argv, char **argenv)
+{
+	pid_t child;
+
+	child = fork();
+	if (child == -1)
+	{
+		perror("Error:");
+		exit(1);
+	}
+
+	if (child == 0)
+	{
+
+		//if (!(execve(argv[0], argv, argenv)));
+		execve(argv[0], argv, argenv);
+		/**
+		 * i added this part in order to printout
+		 * error messages and exit the process
+		 */
+		if (argv[0][0] != '/' && argv[0][0] != '.')
+			printf("%s: command not found\n", argv[0]);
+		else
+			perror(argv[0]);
+		exit(1);
+	}
+
+	else
+	{
+		/**
+		 * i removed the sleeping time because
+		 * it was annoying Ca m’a SOULE’ :D
+		 */
+		wait(NULL);
+	}
+}
